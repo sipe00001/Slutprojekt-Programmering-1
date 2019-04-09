ac0a3476d647e9fca0c0681c91ac0ced7b76b65e
Simon Pettersson <sipe00001@utb.vaxjo.se>
2019-04-09 16:14:26 +0200

    La till kamerafunktionalitet, la till paddeln.

--- Slutprojekt Programmering 1/Slutprojekt Programmering 1/Game1.cs
+++ Slutprojekt Programmering 1/Slutprojekt Programmering 1/Game1.cs
@@ -29,9 +29,108 @@ namespace Slutprojekt_Programmering_1
 
         }
 
-        public void Draw(SpriteBatch spriteBatch)
+        public void Draw(SpriteBatch spriteBatch, Vector2 offset)
         {
-            spriteBatch.Draw(texture, new Vector2(hitbox.X, hitbox.Y), Color.White);
+            spriteBatch.Draw(texture, new Vector2(hitbox.X, hitbox.Y) - offset, Color.White);
+        }
+    }
+	
+/// <summary>
+/// Denna klassen skapar en punkt som bestämmer var saker ska ritas i relation till spelarens vy. 
+/// Kamerans plats uppdateras när paddeln går utanför skärmen. Som om man byter level. 
+/// Detta görs med en animation (Vector2.Lerp).
+/// </summary>
+    class Cam
+    {
+        int level;
+        int transit;
+        float time;
+        private float screenWidth;
+        public Cam(float scrWidth)
+        {
+            screenWidth = scrWidth;
+        }
+        public Vector2 GetVector() {
+			//Ger tillbaka offset för att rita saker på rätt plats
+			//Utvärderar vilken offset vektor som alla draw-objekten ska använda.
+            Vector2 transitVector = new Vector2(transit * screenWidth, 0);
+            Vector2 levelVector = new Vector2(level * screenWidth, 0);
+
+            return Vector2.Lerp(transitVector, levelVector, time);
+        }
+        public float GetTransitionTime()
+        {
+            return time;
+        }
+        public float GetTargetLevelLeftEdge()
+        {
+            return level * screenWidth;
+        }
+        public void Update(Paddle paddle, float dt)
+        {
+            float paddlePos = paddle.getHitbox().Center.X;
+            int nextLevel = (int)(paddlePos / screenWidth); 
+            if (level != nextLevel && time>=1.0f)
+            {
+                transit = level;
+                time = 0.0f;
+                level = nextLevel;
+            }
+            time += dt;
+            time = Math.Min(time, 1.0f);
+
+
+        }
+        
+    }
+/// <summary>
+/// Detta är paddeln. Den kan flyttas av spelaren till höger och vänster. 
+/// Den håller sig på skärmen ifall man inte trycker space. 
+/// Den ritas just nu ut som ett block.
+/// </summary>
+    class Paddle
+    {
+        Texture2D texture;
+        Rectangle hitbox;
+
+        public Paddle(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            hitbox = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+        }
+
+        public Rectangle getHitbox() { return hitbox; }
+
+        public void Update(float targetLevelLeftEdge, bool transitionDone, float screenWidth)
+        {
+            //Styrning
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Right))
+                hitbox.X += 10;
+            if (state.IsKeyDown(Keys.Left))
+                hitbox.X -= 10;
+            if (hitbox.X <= 0.0f)
+            {
+                hitbox.X = 0;
+            }
+            float paddlewidth = hitbox.Width;
+            //Kod för att se till att man stannar på nuvarande nivå tills man vill byta (trycker space)
+            bool allowTransition = transitionDone && state.IsKeyDown(Keys.Space);
+            float leftEdgeBorder = targetLevelLeftEdge - paddlewidth / 2;
+            if (hitbox.X <= leftEdgeBorder && !allowTransition)
+            {
+                hitbox.X = (int)leftEdgeBorder;
+            }
+            float rightEdgeBorder = targetLevelLeftEdge + screenWidth - paddlewidth / 2 - 1;
+            if (hitbox.X >= rightEdgeBorder && !allowTransition)
+            {
+                hitbox.X = (int)(rightEdgeBorder);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 offset)
+        {
+            spriteBatch.Draw(texture, new Vector2(hitbox.X, hitbox.Y) - offset, Color.White);
         }
     }
 
@@ -41,6 +140,8 @@ namespace Slutprojekt_Programmering_1
         SpriteBatch spriteBatch;
         Block[,] blockarray;
         Texture2D blocktexture;
+        Paddle paddle;
+        Cam cam;
         public Game1()
         {
             
@@ -86,6 +187,9 @@ namespace Slutprojekt_Programmering_1
                         blockarray[x,y] = new Block(blocktexture, pos);
                 }
             }
+            Vector2 paddlepos = new Vector2(graphics.PreferredBackBufferWidth * 0.5f, graphics.PreferredBackBufferHeight * 0.9f);
+            paddle = new Paddle(blocktexture, paddlepos);
+            cam = new Cam(graphics.PreferredBackBufferWidth);
         }
 
         /// <summary>
@@ -104,12 +208,15 @@ namespace Slutprojekt_Programmering_1
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
-
+            cam.Update(paddle, dt);
+            paddle.Update(cam.GetTargetLevelLeftEdge(), cam.GetTransitionTime()>=1.0f, graphics.PreferredBackBufferWidth);
             base.Update(gameTime);
+            
         }
 
         /// <summary>
@@ -118,12 +225,13 @@ namespace Slutprojekt_Programmering_1
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-            spriteBatch.Begin();
+           GraphicsDevice.Clear(Color.CornflowerBlue);
             
+            spriteBatch.Begin();
+            paddle.Draw(spriteBatch, cam.GetVector());
             foreach(Block blk in blockarray)
             {
-                blk.Draw(spriteBatch);
+                blk.Draw(spriteBatch, cam.GetVector());
             }
             spriteBatch.End();
 
